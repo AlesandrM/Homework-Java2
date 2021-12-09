50196493ac0e0b1ccbd9e2ca13462cabf3e1e203
Aleksandr Madzharov <madzharov-aleks@mail.ru>
2021-12-09 15:05:28 +0500

    homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args){
+        int price = 2500;
+        int bonus = 10;
+        int mili = price/bonus;
+        System.out.println(mili);
+
+        int mili = 250;
+
+
 
     }
 }
